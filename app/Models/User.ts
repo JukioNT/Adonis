d44804f31276e454da6cdf8242ed275f1b725f68
JukioNT <jjulio2468@gmail.com>
2023-09-25 08:56:56 -0300

    PushNotifications

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -12,6 +12,8 @@ export default class User extends BaseModel {
   @column()
   public email: string
 
+  @column()
+  public token: string
 
   @column({ serializeAs: null })
   public password: string
