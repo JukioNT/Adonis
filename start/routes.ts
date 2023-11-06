8f2fd8a673a5365e2c8b4daa4b5faf0dc91f0464
JukioNT <jjulio2468@gmail.com>
2023-11-06 09:35:36 -0300

    queue

--- start/routes.ts
+++ start/routes.ts
@@ -28,4 +28,5 @@ Route.post("/register", "AuthController.register")
 Route.post("/login", "AuthController.login")
 Route .group(() => {
   Route.put("/user", "UsersContoller.update")
+  Route.post("/push", "SendPushesController.send")
 }).middleware('auth');
