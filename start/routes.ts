d126660fd5e8dbc29d0fc3d569ccf1cac4a2de9b
JukioNT <jjulio2468@gmail.com>
2023-11-06 10:09:05 -0300

    fixes

--- start/routes.ts
+++ start/routes.ts
@@ -7,6 +7,6 @@ Route.get('/', async () => {
 Route.post("/register", "AuthController.register")
 Route.post("/login", "AuthController.login")
 Route .group(() => {
-  Route.put("/user", "UsersContoller.update")
+  Route.put("/user", "UsersController.update")
   Route.post("/push", "SendPushesController.send")
 }).middleware('auth');
